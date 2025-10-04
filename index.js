15f5d9cf4c731dc7f73995e7271e0078b157a777
brianwaithaka <waithakabrian8@gmail.com>
2025-10-04 23:19:54 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+const mainEl = document.querySelector("main#main");
+console.log(mainEl);
+
+mainEl.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Brian is the champion";
+
+console.log(newHeader);
+
